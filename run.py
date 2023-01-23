aa831b91f12e69eb7423875a1c43ca675f8efd36
fenton1000 <fenton1000@yahoo.co.uk>
2023-01-23 15:47:20 +0000

    Get survey questions from the google sheet and print to screen

--- run.py
+++ run.py
@@ -12,9 +12,45 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('electric_car_survey')
 
-questions = SHEET.worksheet('questions')
 
-data = questions.get_all_values()
+def run_survey():
+    """
+    Runs the survey part of the application.
+    Asks each of the questions contained in the questions worksheet in
+    turn by calling the get question function and printing the returned
+    question to screen.
+    """
 
-print(data)
+    questions_worksheet = SHEET.worksheet('questions')
+    number_of_questions = len(questions_worksheet.row_values(1))
+    ind = 1
+    while ind <= number_of_questions:
+        question = get_question(ind)
+
+        print(f'Question {ind}: {question[1]}\n')
+
+        ind += 1
+
+        final_answer_index = len(question) - 1
+        answer_index = 2
+
+        while answer_index <= final_answer_index:
+            answer_number = answer_index - 1
+            print(f'Answer {answer_number}: {question[answer_index]}\n')
+            answer_index += 1
+        
+        chosen_answer = input('Please enter the number for your answer:')
+
+
+def get_question(ind):
+    """
+    Imports the required survey question from the questions worksheet
+    """
+    questions_worksheet = SHEET.worksheet('questions')
+    question = questions_worksheet.col_values(ind)
+
+    return question
+
+
+run_survey()
 
